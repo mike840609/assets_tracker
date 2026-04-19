d2570f1194a2e3f728bd72618d855807b357193f
Mike Tsai <mike840609@gmail.com>
2026-04-20 01:39:11 +0800

    feat: add @next/bundle-analyzer and documentation

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig: NextConfig = {
   cacheComponents: true,
   images: {
@@ -34,4 +38,4 @@ const nextConfig: NextConfig = {
   ],
 };
 
-export default withNextIntl(nextConfig);
+export default withBundleAnalyzer(withNextIntl(nextConfig));
